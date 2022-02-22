f807892d7380ee59cfe23dbc42797efaf11c7e13
Alan Montoya <gazalem@hotmail.com>
2022-02-21 22:56:04 -0500

    Site Setup and Mockup of Assigment

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Global variables
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let pageLastModified = new Date(document.lastModified)
+
+// insert the current year to the document
+document.getElementById("yearJS").textContent = currentYear;
+// insert the document last modifed date
+try {
+    let options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+    document.getElementById("lastUpdated").textContent = pageLastModified.toLocaleDateString("en-US", options);
+} catch (error) {
+    alert("The browser does not support Date locale")
+}
